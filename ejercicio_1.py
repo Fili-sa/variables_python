a414a7e8872d23340c3a946d044832481e89aa97
Fili-sa <116232815+Fili-sa@users.noreply.github.com>
2022-10-31 02:03:19 -0300

    Add files via upload
    hola, como estan? consulta queria saber si asi el resultado esta bien?

---
+++ ejercicio_1.py
@@ -0,0 +1,34 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+# Realizar la suma de las dos variables
+# Crear una variable llamada suma donde se 
+# almacene el valor de la suma de las variable numero_1 y numero_2
+# ej:
+suma = numero_1 + numero_2
+
+
+# Imprimir en pantalla el resultado de la suma
+print("el resultado de la suma es" , suma) 
+
+# Repita el procedimiento para realizar la resta
+# Crear una variable llamada resta donde se 
+resta = numero_1 - numero_2
+
+#Imprimir en pantalla el resultado de la resta
+print("resulatado de la resta", resta)
+
+divicion = numero_1 / numero_2
+
+print("resultado de la divicion es" , divicion)
